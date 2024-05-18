f17d18d7ad3edcaa1121159f30f687249f3bd0f8
christyanbrayan <christyankev@gmail.com>
2024-05-17 23:17:23 -0300

    change to inline css

--- index.php
+++ index.php
@@ -4,7 +4,50 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Escolha seu Jogo</title>
-  <link rel="stylesheet" href="style.css">
+  <style>
+  body {
+    background-color: #ddd;
+    font-family: Tahoma, Verdana, sans-serif;
+  }
+
+  div#corpo {
+    width: 800px;
+    margin: auto;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0px 0px 30px #777;
+  }
+
+  table.listagem {
+    width: 100%;
+    text-align: left;
+    /* tira espacos */
+    border-collapse: collapse;
+  }
+
+  table.listagem tr {
+    height: 50px;
+  }
+
+  table.listagem td {
+    /* border: 1px solid #ddd; */
+    padding: 10px;
+  }
+
+  /* impar */
+  table.listagem tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+
+  /* primeira coluna */
+  table.listagem td:nth-child(1) {
+    width: 50px;
+  }
+
+  table.listagem td:nth-child(3) {
+    width: 90px;
+  }
+  </style>
 </head>
 <body>
 
