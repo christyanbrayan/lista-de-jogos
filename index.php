ddb3ea605c125a897a116f0644a15c8f649253cf
christyanbrayan <christyankev@gmail.com>
2024-05-17 22:20:40 -0300

    add basic html and css structure

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Escolha seu Jogo</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <div id="corpo">
+    <h1>Escolha seu Jogo</h1>
+    <table class="listagem">
+      <tr>
+        <td>Foto</td>
+        <td>Nome</td>
+        <td>Adm</td>
+      </tr>
+      <tr>
+        <td>Foto</td>
+        <td>Nome</td>
+        <td>Adm</td>
+      </tr>
+      <tr>
+        <td>Foto</td>
+        <td>Nome</td>
+        <td>Adm</td>
+      </tr>
+      <tr>
+        <td>Foto</td>
+        <td>Nome</td>
+        <td>Adm</td>
+      </tr>
+    </table>
+  </div>
+
+</body>
+</html>
